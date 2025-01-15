9576f611563f0d0f2b5e0f0a83599b77c84d65f7
Gabriel Henrique <gabriel.henrique@agenciam3.com>
2025-01-14 23:27:24 -0300

    :tada: First commit :tada:

---
+++ src/main/java/br/com/gabrielghvn/todolist/user/UserModel.java
@@ -0,0 +1,32 @@
+package br.com.gabrielghvn.todolist.user;
+
+public class UserModel {
+  
+  String username;
+  String name;
+  String password;
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getName() {
+    return name;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+}
